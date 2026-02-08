6ca560f9d46e7e87eef87c8e25b406f855734572
kjjr7329-dotcom <kjjr7329@gmail.com>
2026-02-08 12:51:29 +0900

    사진수정

--- src/data/activitiesData.ts
+++ src/data/activitiesData.ts
@@ -9,21 +9,21 @@ export const activitiesData = [
     imageUrl: "/images/activity-11.jpg", // public/images 폴더에 넣은 파일 이름!
   },
   {
-    id: 1,
+    id: 2,
     title: "쑥쑥 자란 꼬물이들",
     description: "큰 소리에 놀라지 않는 사자와 같이,그물에 걸리지 않는 바람과 같이,흙탕물에 더럽혀지지 않는 연꽃과 같이, 무소의 뿔처럼 혼자서 가라. -숫타니파타-",
     date: "2025년 11월",
     imageUrl: "/images/activity-09.jpg", // public/images 폴더에 넣은 파일 이름!
   },
   {
-    id: 1,
+    id: 3,
     title: "바이크 투어",
     description: "가을 바람을 느끼며 영주~예천~문경 일원을 달리다",
     date: "2025년 10월",
     imageUrl: "/images/activity-01.png", // public/images 폴더에 넣은 파일 이름!
   },
   {
-    id: 2,
+    id: 4,
     title: "제주도",
     description: "한라산, 사라오름, 흙돼지 맛집",
     date: "2025년 5월",
