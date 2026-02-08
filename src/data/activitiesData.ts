dc41ec74a22f54f9526d0e2342a8bc5dccb36af0
kjjr7329-dotcom <kjjr7329@gmail.com>
2026-02-08 12:48:34 +0900

    사진수정

--- src/data/activitiesData.ts
+++ src/data/activitiesData.ts
@@ -3,17 +3,31 @@
 export const activitiesData = [
   {
     id: 1,
+    title: "2026년 새해,붉은 말의 해",
+    description: "5년후, 우리는 어떤 모습을 하고 있을까? 이런 작은 행복을 함께나누길..씩씩하고 지혜롭게 지내주길..",
+    date: "2026년 1월",
+    imageUrl: "/images/activity-11.jpg", // public/images 폴더에 넣은 파일 이름!
+  },
+  {
+    id: 1,
     title: "쑥쑥 자란 꼬물이들",
     description: "큰 소리에 놀라지 않는 사자와 같이,그물에 걸리지 않는 바람과 같이,흙탕물에 더럽혀지지 않는 연꽃과 같이, 무소의 뿔처럼 혼자서 가라. -숫타니파타-",
-    date: "2025-11-20",
+    date: "2025년 11월",
     imageUrl: "/images/activity-09.jpg", // public/images 폴더에 넣은 파일 이름!
   },
   {
+    id: 1,
+    title: "바이크 투어",
+    description: "가을 바람을 느끼며 영주~예천~문경 일원을 달리다",
+    date: "2025년 10월",
+    imageUrl: "/images/activity-01.png", // public/images 폴더에 넣은 파일 이름!
+  },
+  {
     id: 2,
-    title: "유튜브 촬영 중",
-    description: "사자성어 숏츠 기획 회의를 했다. 호랑이 캐릭터가 너무 귀엽다.",
-    date: "2024-06-01",
-    imageUrl: "/images/activity-11.jpg",
+    title: "제주도",
+    description: "한라산, 사라오름, 흙돼지 맛집",
+    date: "2025년 5월",
+    imageUrl: "/images/activity-06.jpg",
   },
   // ... 이런 식으로 계속 추가하면 됩니다 (콤마 , 잊지 마세요!)
 ];
